dab527ce4502cfe671547358b1135e04b1d2d536
Alejandro B <alejandro.bujan.pampin@gmail.com>
2020-03-16 00:38:39 +0100

    Primera versión del módulo

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,34 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mi_musica",
+    'name': "Mi Musica",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Gestiona canciones, artistas y álbumes.",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': "Módulo para la gestión de música. Trabaja con canciones, artistas y álbumes. Desarrollado para el módulo de SXE en IES San Clemente en 2020.",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Alejandro Buján",
+    'website': "https://github.com/alejandrobujan/mi_musica",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Extra Tools',
+    'version': '12.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/music_album.xml',
+        'views/music_artist.xml',
+        'views/music_track.xml'
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
 }
--- models/models.py
+++ models/models.py
@@ -1,15 +1,58 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
-# class mi_musica(models.Model):
-#     _name = 'mi_musica.mi_musica'
+from datetime import datetime, timedelta
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+
+logger = logging.getLogger(__name__)
+
+class MusicAlbum(models.Model):
+    _name = 'music.album'
+    _description = 'Álbum Musical'
+
+    name = fields.Char('Album', required=True)
+    date_release = fields.Date('Fecha de Lanzamiento')
+    album_image = fields.Binary('Carátula')
+    artist_id = fields.Many2one('music.artist', string='Artista')
+    track_ids = fields.One2many('music.track', string="Canciones", inverse_name='album_id')
+
+class MusicGender(models.Model):
+    _name = 'music.gender'
+    _description = 'Género Musical'
+
+    name = fields.Char('Género', required=True)
+    description = fields.Text('Descripción')
+
+class MusicArtist(models.Model):
+    _name = 'music.artist'
+    _inherits = {'res.partner': 'partner_id'}
+    _description = 'Artista Musical'
+
+    partner_id = fields.Many2one('res.partner', string='Artista', ondelete='cascade')
+    member_image = fields.Binary('Imagen', related='partner_id.image')
+    date_start = fields.Date('En activo desde')
+    date_of_birth = fields.Date('Fecha de Nacimiento')
+
+class MusicTrack(models.Model):
+    _name = 'music.track'
+    _description = 'Canción'
+
+    name = fields.Char('Canción', required=True)
+    gender_id = fields.Many2one('music.gender', string='Género')
+    artist_ids = fields.Many2many('music.artist', string='Artistas')
+    album_id =  fields.Many2one('music.album', string='Álbum')
+    date_release = fields.Date('Fecha de Lanzamiento')
+
+    album_image = fields.Binary('Imagen del Álbum', related='album_id.album_image')
+    
+    @api.constrains('artist_ids')
+    def _check_date_release(self):
+        for record in self:
+            artists = record.artist_ids
+            for artist in artists:
+                if record.date_release < artist.date_start:
+                    raise models.ValidationError('¡El lanzamiento de la canción no pudo ser antes del comienzo del artista!')
